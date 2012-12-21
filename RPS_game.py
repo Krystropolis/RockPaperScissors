46b85a0577000fc07494993badea799b453e680a
Krystropolis <e.krystal@gmail.com>
2012-12-20 22:14:01 -0500

    debugged

--- RPS_game.py
+++ RPS_game.py
@@ -1,45 +1,44 @@
 import random
 
 choices = ["rock", "paper", "scissors"]
-
 computer_choice = random.choice(choices)
-player_input = raw_input("rock, paper, or scissors? ")
-player_choice = player_input.lower()
 
 def game():
-	
 	global computer_choice
-	global player_choice
 
-	if player_choice == computer_choice:
-		print "You chose " + player_choice + "."
-		print "The computer chose " + computer_choice + "."
-		print "TIE!"
-	elif player_choice == "rock" and computer_choice == "paper":
-		print "You chose " + player_choice + "."
-		print "The computer chose " + computer_choice + "."
-		print "The computer won! :("
-	elif player_choice == "rock" and computer_choice == "scissors":
-		print "You chose " + player_choice + "."
-		print "The computer chose " + computer_choice + "."
-		print "You won! :)"
-	elif player_choice == "paper" and computer_choice == "rock":
-		print "You chose " + player_choice + "."
-		print "The computer chose " + computer_choice + "."
-		print "You won! :)"
-	elif player_choice == "paper" and computer_choice == "scissors":
-		print "You chose " + player_choice + "."
-		print "The computer chose " + computer_choice + "."
-		print "The computer won! :("
-	elif player_choice == "scissors" and computer_choice == "paper":
-		print "You chose " + player_choice + "."
-		print "The computer chose " + computer_choice + "."
-		print "You won! :)"
-	elif player_choice == "scissors" and computer_choice == "rock":
-		print "You chose " + player_choice + "."
-		print "The computer chose " + computer_choice + "."
-		print "The computer won! :("
+	player_choice = raw_input("rock, paper, or scissors? ").lower()
+
+	if player_choice != "rock" and player_choice != "paper" and player_choice != "scissors":
+		print "That is not a valid choice."
+		game()
 	else:
-		print False
+		if player_choice == computer_choice:
+			print "You chose " + player_choice + "."
+			print "The computer chose " + computer_choice + "."
+			print "TIE!"
+		elif player_choice == "rock" and computer_choice == "paper":
+			print "You chose " + player_choice + "."
+			print "The computer chose " + computer_choice + "."
+			print "The computer won! :("
+		elif player_choice == "rock" and computer_choice == "scissors":
+			print "You chose " + player_choice + "."
+			print "The computer chose " + computer_choice + "."
+			print "You won! :)"
+		elif player_choice == "paper" and computer_choice == "rock":
+			print "You chose " + player_choice + "."
+			print "The computer chose " + computer_choice + "."
+			print "You won! :)"
+		elif player_choice == "paper" and computer_choice == "scissors":
+			print "You chose " + player_choice + "."
+			print "The computer chose " + computer_choice + "."
+			print "The computer won! :("
+		elif player_choice == "scissors" and computer_choice == "paper":
+			print "You chose " + player_choice + "."
+			print "The computer chose " + computer_choice + "."
+			print "You won! :)"
+		elif player_choice == "scissors" and computer_choice == "rock":
+			print "You chose " + player_choice + "."
+			print "The computer chose " + computer_choice + "."
+			print "The computer won! :("
 
 game()
